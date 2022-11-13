9955347ca3ed7b7172b3dbdf490b382b36ab5f1c
bwohl <65831594+bwohl@users.noreply.github.com>
2022-11-13 21:10:46 +0100

    setup pyproject.toml and versioning

--- pyVenus/__init__.py
+++ pyVenus/__init__.py
@@ -2,3 +2,5 @@ from .connection import Connection
 from .data_types import Variable, Array, Sequence, Device, Liquidclass
 from .resources import Resources
 from .helpers import Helpers
+
+__version__ = "1.0.0"
