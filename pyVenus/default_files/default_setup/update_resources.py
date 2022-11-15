52ec600ad327f0a8491d35efdc35f93722e6260c
bwohl <65831594+bwohl@users.noreply.github.com>
2022-11-15 20:34:16 +0100

    setup files for cli scripts

---
+++ pyVenus/default_files/default_setup/update_resources.py
@@ -0,0 +1,5 @@
+from pyVenus import Resources
+
+Resources.read_layout("add_name_of_your_deck_layout.lay")
+Resources.read_submethods()
+Resources.read_liquid_classes(True,False,True,False,False)
---
+++ pyVenus/scripts.py
@@ -0,0 +1,6 @@
+def setup_example_method():
+    print("setup example method")
+    pass
+
+def init():
+    print("init")
